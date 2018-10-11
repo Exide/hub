2621574ecb24beef1c5ba20887ac0b1b02ea7549
Ryan Zander <exide@hotmail.com>
2018-10-11 01:39:09 -0700

    Organized configs for easier Docker building.

--- localDocker.sh
+++ localDocker.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+
+[[ ! -z "$1" ]] && PREFIX=$1 || PREFIX="default"
+
 set -euo pipefail errfail
 
 echo "Cleaning up any old files."
 rm -rf docker/hub
+rm -rf docker/hub.properties
+rm -rf docker/logback.xml
 
 echo "Building the code into a tarball."
 gradle clean compileJava distTar
@@ -12,6 +17,12 @@ tar -xvf build/distributions/hub-*.tgz -C docker/
 
 echo "Putting the hub code where the Dockerfile wants it."
 mv docker/hub-*/ docker/hub
+
+echo "Adding the config files matching: ${PREFIX}"
+cp docker/configs/${PREFIX}* docker/
+for file in docker/${PREFIX}*; do
+  mv "$file" "${file/${PREFIX}-/}"
+done
 
 echo "Building the docker image."
 docker build --tag hub:local docker
